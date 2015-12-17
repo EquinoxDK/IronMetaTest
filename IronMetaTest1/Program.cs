1572c2d863578ec8b5575bc579fc70ab9c0552fe
Jan Skrubbeltrang <jan.skrubbeltrang@gmail.com>
2015-12-17 17:35:13 +0100

    Initial Commit

---
+++ IronMetaTest1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IronMetaTest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            calculatorTest peg = new calculatorTest();
+            Stopwatch stopwatch = new Stopwatch();
+            string parseString = "x";
+            Console.WriteLine("Type in math expression to solve (Enter to end)");
+            while (!parseString.ToLower().Equals(""))
+            {
+                Console.Write(": ");
+                parseString = Console.ReadLine();
+                if (!parseString.Equals(""))
+                {
+                    stopwatch.Reset();
+                    stopwatch.Start();
+                    var result = peg.GetMatch(parseString, peg.Expression);
+                    stopwatch.Stop();
+                    if (result.Success)
+                    {
+                        Console.WriteLine("Result: " + result.Result);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error: " + result.Error);
+                    }
+                    Console.WriteLine("Time (ms): " + stopwatch.ElapsedMilliseconds);
+                }
+            }
+
+        }
+    }
+}
